6f7524e1638cf8eff1bd7e5a41d43eb37d961d79
Anh-vu <anhuvdinh@gmail.com>
2021-07-27 21:49:02 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector("main").remove();
+
+const newHeader =  document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "<p>YOUR-NAME is the champion</p>";
